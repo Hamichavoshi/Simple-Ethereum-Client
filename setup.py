2c704a0d1d73b5b8441b9c80a820b10842acba09
fahadh4ilyas <fahadhilyas4@gmail.com>
2022-03-31 21:31:01 +0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from distutils.core import setup
+
+from eth_client import __version__
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="Simple Ethereum Client",
+    version=__version__,
+    description="simple python client to access ethereum network",
+    long_description=read('README.md'),
+    author="Ahmad Fahadh Ilyas",
+    author_email="fahadhilyas4@gmail.com",
+    url="https://github.com/fahadh4ilyas/Simple-Ethereum-Client.git",
+    packages=["eth_client"],
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Utilities"
+    ],
+    keywords=["ethereum", "web3", "solidity"],
+    install_requires=[
+        "py-solc-x>=1.1.0",
+        "web3>=5.20.0"
+    ],
+    python_requires=">=3.7,<3.11"
+)
